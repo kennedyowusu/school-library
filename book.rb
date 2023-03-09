6725a6bbd80885933beb9c9f6e2ffdc3112c5aec
kennedyowusu <kennediowusu@gmail.com>
2023-03-09 19:21:42 +0000

    implement reviewer suggestions

--- book.rb
+++ book.rb
@@ -7,7 +7,20 @@ class Book
     @rentals = []
   end
 
-  def add_rentals(rentals)
-    @rentals += rentals
+  def add_rental(person, date)
+    rental = Rental.new(date, person, self)
+    @rentals.push(rental)
+  end
+end
+
+class Rental
+  attr_accessor :date, :book, :person
+
+  def initialize(date, person, book)
+    @date = date
+    @book = book
+    @person = person
+    book.rentals.push(self)
+    person.rentals.push(self)
   end
 end
