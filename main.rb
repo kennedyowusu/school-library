6636582575efd66339e50613cbcd0a44a48ff3df
Kennedy Owusu <32777408+kennedyowusu@users.noreply.github.com>
2023-03-10 09:25:05 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,42 +1,76 @@
 require './app'
 
+def display_options
+  puts "\nPlease choose an option by Entering a Number: "
+  puts [
+    '',
+    ' [1] - List all books',
+    ' [2] - List all people',
+    ' [3] - Create a person',
+    ' [4] - Create a book',
+    ' [5] - Create a rental',
+    ' [6] - List all rentals for a person',
+    ' [7] - Exit',
+    ''
+  ]
+end
+
+def handle_list_books(app)
+  app.list_books
+end
+
+def handle_list_people(app)
+  app.list_people
+end
+
+def handle_create_person(app)
+  app.create_person
+end
+
+def handle_create_book(app)
+  app.create_book
+end
+
+def handle_create_rental(app)
+  app.create_rental
+end
+
+def handle_list_rentals_for_person(app)
+  app.list_rentals_for_person
+end
+
+def handle_exit
+  puts 'Goodbye!'
+  exit
+end
+
+def handle_invalid_option
+  puts 'Invalid option!'
+end
+
+def handle_option(app, option)
+  case option
+  when 1 then handle_list_books(app)
+  when 2 then handle_list_people(app)
+  when 3 then handle_create_person(app)
+  when 4 then handle_create_book(app)
+  when 5 then handle_create_rental(app)
+  when 6 then handle_list_rentals_for_person(app)
+  when 7 then handle_exit
+  else handle_invalid_option
+  end
+end
+
 def main
   app = App.new
-  exit_button = true
-  puts "\nWelcome to School Library App!"
-  while exit_button
-    options = [
-      '',
-      '   [1] - List all books',
-      '   [2] - List all people',
-      '   [3] - Create a person',
-      '   [4] - Create a book',
-      '   [5] - Create a rental',
-      '   [6] - List all rentals a given person id',
-      '   [7] - Exit',
-      ''
-    ]
-    puts "\nPlease choose an option by entering a number:"
-    puts options
+  puts "\nWelcome to the School Library App!"
+
+  loop do
+    display_options
     print 'Answer: '
-    choice = gets.chomp
-    case choice
-    when '1'
-      app.list_books
-    when '2'
-      app.list_people
-    when '3'
-      app.create_person
-    when '4'
-      app.create_book
-    when '5'
-      app.create_rental
-    when '6'
-      app.list_rentals
-    else
-      puts 'Goodbye!'
-      exit_button = false
-    end
+    choice = gets.chomp.to_i
+
+    handle_option(app, choice)
   end
 end
 
