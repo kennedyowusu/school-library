040444474c8598402c5af1e1e7b94cf3a8e978de
Kennedy Owusu <32777408+kennedyowusu@users.noreply.github.com>
2023-03-10 09:27:30 +0000

    Update nameable.rb

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,11 @@
 class Nameable
+  attr_reader :class_name
+
+  def initialize
+    @class_name = self.class.name
+  end
+
   def correct_name
-    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    raise NotImplementedError, "#{@class_name} does not implement '#{__method__}' method"
   end
 end
